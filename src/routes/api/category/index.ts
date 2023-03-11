0be0ab42c29add3d5c86baed4c5ebf25a4b079b1
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-03-10 22:13:34 -0300

    Added basic structure for routing

---
+++ src/routes/api/category/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// controller
+
+//dtos
+
+//validate
+
+/*router.get
+router.delete
+router.patch
+router.post*/
+
+export default router;
